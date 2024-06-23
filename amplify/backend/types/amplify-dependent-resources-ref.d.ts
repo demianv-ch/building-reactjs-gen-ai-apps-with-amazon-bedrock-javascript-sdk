427b9441eb7efe87275ce30371271f91312d4476
demian vselko <demian.vselko@cloudhesive.com>
2024-06-23 13:48:02 -0300

    amplify init and configure

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "buildingreactjsgenai15dc9007": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
